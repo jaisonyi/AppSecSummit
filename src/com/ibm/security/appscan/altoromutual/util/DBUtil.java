6ded784ef59ed77802e00cb94fe7b705380e8036
j@50nl33 <jaisonyi@gmail.com>
2026-07-01 12:27:12 +0800

    Update error logging comment for clarity

--- src/com/ibm/security/appscan/altoromutual/util/DBUtil.java
+++ src/com/ibm/security/appscan/altoromutual/util/DBUtil.java
@@ -532,7 +532,7 @@ public class DBUtil {
 		} catch (SQLException e){
 			Log4AltoroJ.getInstance().logError(e.getMessage());
 			return -1;
-			// comment 
+			// comment for testing only purpose 
 		}
 	}
 }
